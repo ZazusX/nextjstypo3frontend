cca714e8de6323e934516951e81f6112f8ff3f11
Jan Boldis <jboldis.forwarding@gmail.com>
2023-03-16 09:36:35 +0100

    gallery

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,13 @@
-function Image() {
+import Image from "next/image";
+
+function BlurImage() {
   return (
-    <>
-      <div>
-        <div class="text-xs text-slate-600 uppercase font-bold tracking-wider">{{ eyebrow }}</div>
-        <div class="font-bold text-slate-700 leading-snug"></div>
-        <div class="mt-2 text-sm text-slate-600">{{ pricing }}</div>
+    <a href="#" className="group">
+      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <Image alt="" src="https://bit.ly/placeholder-img" layout="fill" objectFit="cover" className="group-hover:opacity-75" />
       </div>
-    </>
+      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+    </a>
   );
 }
-
-export default {
-  props: ["img", "imgAlt", "eyebrow", "title", "pricing", "url"],
-};
